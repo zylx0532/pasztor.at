0f8b39af265cdc0b7f8838b47ca06d0e0504b299
János Pásztor <janos@pasztor.at>
2018-10-12 19:54:55 +0200

    Deploy user

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -4,5 +4,5 @@ set -e
 
 for NODE in $(dig +short all.cdn.opsbears.net); do
     echo "Deploying to ${NODE}..."
-    rsync -e "ssh" --rsync-path="sudo rsync" -az --delete ./_site/ $NODE:/srv/www/pasztor.at/htdocs/
+    rsync -az --delete ./_site/ deploy@$NODE:/srv/www/pasztor.at/htdocs/
 done
