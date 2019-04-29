b96b6b6576d1853771cda9355d23188e85b7ffb1
János Pásztor <janos@pasztor.at>
2019-04-29 08:40:15 +0200

    Deployment

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
-set -e
-
-for NODE in $(dig +short all.cdn.opsbears.net); do
-    echo "Deploying to ${NODE}..."
-    rsync -az --delete ./_site/ deploy@$NODE:/srv/www/pasztor.at/htdocs/
-done
+pip install s3cmd
+if [[ $? -ne 0 ]]; then
+    exit $?
+fi
+echo "[default]
+host_base = sos-${EXOSCALE_REGION}.exo.io
+host_bucket = %(bucket)s.sos-${EXOSCALE_REGION}.exo.io
+access_key = ${EXOSCALE_KEY}
+secret_key = ${EXOSCALE_SECRET}
+use_https = True
+" > ~/.s3cfg
+s3cmd sync --delete-removed ./_site/ deploy@$NODE:/srv/www/pasztor.at/htdocs/
+RESULT=$?
+rm ~/.s3cfg
+exit ${RESULT}
